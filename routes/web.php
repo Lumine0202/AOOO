920ea5f84194e77f054b93058f09f805a3b7c341
Lumine0202 <lara.dzal@gmail.com>
2025-04-25 20:18:54 +0200

    temp migrate

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ErettsegiController;
 
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Http\Request;
+
+
 Route::get('/', [ErettsegiController::class, 'index'])->name('erettsegi.index');
 Route::post('/groups', [ErettsegiController::class, 'storeGroup'])->name('groups.store');
 Route::post('/erettsegi', [ErettsegiController::class, 'store'])->name('erettsegi.store');
@@ -10,4 +14,12 @@ Route::delete('/erettsegi/{erettsegi}', [ErettsegiController::class, 'destroy'])
 Route::get('/erettsegi/{erettsegi}', [ErettsegiController::class, 'show'])->name('erettsegi.show');
 Route::get('/erettsegi/{erettsegi}/edit', [ErettsegiController::class, 'edit'])->name('erettsegi.edit');
 Route::put('/erettsegi/{erettsegi}', [ErettsegiController::class, 'update'])->name('erettsegi.update');
+
+
+
+
+Route::get('/migrate', function (Request $request) {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migrations ran!';
+})->withoutMiddleware([\Illuminate\Session\Middleware\StartSession::class]);
 
